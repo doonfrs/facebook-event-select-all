d6951f5c8538dd3cf8f391e8a6f4edc9e988c9fb
Feras Abdalrahman <doonfrs@gmail.com>
2024-04-18 01:17:49 +0300

    Update select.js

--- select.js
+++ select.js
@@ -1,15 +1,16 @@
 var i=0;
-document.querySelector('[role="dialog"][aria-label="Invite followers"]').querySelectorAll('div[aria-label]').forEach(async function(e){
+var dialog = document.querySelector('[role="dialog"][aria-label="Invite followers"]');
+if(!dialog)
+{
+    dialog =document.querySelector('[role="dialog"][aria-label="Invite people"]')
+}
+dialog.querySelectorAll('div[aria-label]').forEach(async function(e){
     var p = e.parentElement.parentElement.parentElement;
     if(!p.ariaDisabled && p.role=='button' && p.querySelector('i[data-visualcompletion="css-img"') && p.querySelector('circle'))
     {
         i++;
         if(i>50){
             return;
-        }
-        if(document.querySelector('[role="dialog"][aria-label="Invite followers"]').querySelectorAll('div[aria-label="Remove"]').length>=50)
-        {
-            return
         }
         p.click();
         
